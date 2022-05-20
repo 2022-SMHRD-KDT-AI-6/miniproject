a5b0f7277444e59f905333fbf957a262b702aa0b
smhrd <smhrd@DESKTOP-HTM2M3L>
2022-05-20 17:25:36 +0900

    인성3333

--- GitTest/src/a최종.java
+++ GitTest/src/a최종.java
@@ -4,6 +4,7 @@ public class a최종 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("asdlkfjsdlkfjlkdsjflk");
+		System.out.println("커밋확인");
 	}
 
 }
